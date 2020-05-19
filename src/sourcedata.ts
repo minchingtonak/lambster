4ef5239b794d29a9994b6a398214f3cd9215aa3f
Alec Minchington <alec.minch@gmail.com>
2020-05-19 02:11:09 -0400

    Meant to commit this in the error printing commit

--- src/sourcedata.ts
+++ src/sourcedata.ts
@@ -1,3 +1,16 @@
-export module SourceData {
-    export let current_source: string = '';
+class SourceData {
+    private source: string = "";
+    private sourcelines: string[] = [];
+    setSource(source: string) {
+        this.source = source;
+        this.sourcelines = this.source.split("\n");
+    }
+    getSource(): string {
+        return this.source;
+    }
+    getSourceLine(line: number): string {
+        return this.sourcelines[line - 1];
+    }
 }
+
+export default new SourceData();
